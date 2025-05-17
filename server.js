46d0d4f54ad64bcab53b0ef9b82fab274503b2dd
Sara <sara-jassim@hotmail.com>
2025-05-17 16:19:40 +0300

    17May25

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT ? process.env.PORT : 5000
 
 const db = require("./db")
 
-// const authRouter = require("./routes/authRouter.js")
-// const userRouter = require("./routes/userRouter.js")
+const authRouter = require("./routes/authRouter.js")
+const userRouter = require("./routes/userRouter.js")
 const bookRouter = require("./routes/bookRouter.js")
-// const reviewRouter = require("./routes/reviewRouter.js")
+const reviewRouter = require("./routes/reviewRouter.js")
 
 const app = express()
 
