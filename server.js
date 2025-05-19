fb4002844cc5b39490d3b759616fdc999baa432c
Malak <malakalmahari7042@gmail.com>
2025-05-19 21:19:19 +0300

    added views for user and auth

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan")
 const methodOverride = require("method-override")
 const session = require("express-session")
 require('dotenv').config()
+const MongoStore = require("connect-mongo")
 
 const PORT = process.env.PORT ? process.env.PORT : 5000
 
@@ -15,6 +16,8 @@ const reviewRouter = require("./routes/reviewRouter.js")
 
 const app = express()
 
+app.use(express.static('public'))
+
 app.use(logger("dev"))
 
 app.use(express.json())
@@ -22,11 +25,21 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(methodOverride("_method"))
-app.use(session({
+app.use(
+  session({
   secret: process.env.SECRET_SESSION,
   resave: false,
-  saveUninitialized: true
-}))
+  saveUninitialized: true,
+  store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+  })
+})
+);
+
+app.use((req, res, next) => {
+  res.locals.user = req.session.user
+  next()
+})
 
 app.use('/auth', authRouter)
 
@@ -35,8 +48,9 @@ app.use('/users', userRouter)
 app.use("/books", bookRouter)
 
 app.use("/reviews", reviewRouter)
+
 app.get("/", (req, res) => {
-    res.send('Our app is connected . . . ')
+    res.render('index.ejs')
   })
 
 app.listen(PORT, () => {
